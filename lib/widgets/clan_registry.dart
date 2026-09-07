29a7864056fe10d8f2bd477ae3beb323356fd641
hermes <hermes@local>
2026-09-07 21:26:41 +0000

    Fix clan_registry import paths (widgets/ -> screens/)

--- lib/widgets/clan_registry.dart
+++ lib/widgets/clan_registry.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../data/family_repository.dart';
-import 'family_screen.dart';
-import 'person_screen.dart';
+import '../screens/family_screen.dart';
+import '../screens/person_screen.dart';
 
 /// App-wide registry of all loaded clans (main + side clans), looked up by
 /// [FamilyRepository.clanKey]. Provided above [HomeScreen]; screens use
